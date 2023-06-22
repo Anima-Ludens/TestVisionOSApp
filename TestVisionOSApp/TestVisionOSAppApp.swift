db04092a7b893aab6666c0ebebb9efd10bbcb22a
Fernando Caye <fernando.caye@cellid.com>
2023-06-22 17:17:25 +0900

    Initial Commit

---
+++ TestVisionOSApp/TestVisionOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVisionOSAppApp.swift
+//  TestVisionOSApp
+//
+//  Created by Max Planck on 2023/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TestVisionOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
